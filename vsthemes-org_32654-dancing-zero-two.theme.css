f4cc8d4b5f674065f4a92614924ed5fef8c3dda3
yzkgetto <129540688+yzkgetto@users.noreply.github.com>
2023-03-31 14:11:47 -0300

    Add files via upload

---
+++ vsthemes-org_32654-dancing-zero-two.theme.css
@@ -0,0 +1,133 @@
+/**
+ * @name DancingZeroTwo
+ * @author [LVPD] Vladimir_Vasilevich[1]#3899 | Bada774
+ * @version 2.0.0
+ * @description Тема танцующей ZeroTwo из клипа 2 Phut Hon (KAIZ Remix)
+ * @source https://github.com/DiscordStyles/FrostedGlass
+ * @authorLink https://discordhub.com/profile/360111380059389964
+ * @authorId 360111380059389964
+*/
+@import url("https://puckzxz.github.io/NotAnotherAnimeTheme/build/v3/naat.v3.css");
+@import url('https://discordstyles.github.io/FrostedGlass/dist/FrostedGlass.css');
+@import url('https://discordstyles.github.io/Addons/windows-titlebar.css');
+@import url('https://discordstyles.github.io/RadialStatus/dist/RadialStatus.css');
+
+:root {
+  /*FrostedGlass*/
+  --background-image: url('https://i.imgur.com/WaG1PAu.gif');
+  --background-image-blur: 0px;
+  --background-image-size: cover;
+  --background-image-position: center;
+  --small-user-popout-background-image: url('https://i.imgur.com/Jvf5abf.gif');
+  --small-user-popout-background-transparency: #00000099;
+  --small-user-popout-background-image-position: top;
+  --home-button-image: url('https://99px.ru/sstorage/86/2018/09/image_861609180322028143900.gif');
+  --home-button-size: cover;
+  --home-button-position: center;
+  --serverlist-brightness: 0;
+  --left-brightness: 0;
+  --middle-brightness: 0;
+  --right-brightness: 0;
+  --popout-modal-brightness: 0.52;
+  --gradient-primary: 151,25,123;
+  --gradient-secondary: 179,0,107;
+  --link-colour: #FFC107;
+  --scrollbar-colour: #FF0DBEB4;
+  --gradient-direction: 299deg;
+  --tint-colour: 160,41,136;
+  --tint-brightness: 0.3;
+  --font: 'Noto Serif','serif';
+  --window-padding: 0px;
+  --window-roundness: 14px;
+  --show-gift-gif-buttons: none;
+  --update-notice-1: none;
+  --rs-small-spacing: 2px;
+  --rs-medium-spacing: 0px;
+  --rs-large-spacing: 0px;
+  --rs-small-width: 2.5px;
+  --rs-medium-width: 2.5px;
+  --rs-large-width: 2.5px;
+  --rs-avatar-shape: 22.5%;
+  --rs-online-color: #FF00AF;
+  --rs-idle-color: #faa61a;
+  --rs-dnd-color: #F00000;
+  --rs-offline-color: #5C00AA;
+  --rs-streaming-color: #2CEEDD;
+  --main-theme-color: #FF0DBEB4;
+  /*NotAnotherAnime*/
+--server-listing-width: 72px;
+--font-size: 100%;
+--category-hover-color: FF0DBEB4;
+--unread-server-animation: rainbow;
+--video-screen-button-background-color: #8B163FB4;
+--video-screen-button-foreground-color: #ffffffff;
+--video-screen-button-background-hovor-color: #8B163FB4;
+--video-screen-button-foreground-hovor-color: #ffffffff;
+--member-listing-role-color: #FF0DBEB4;
+--code-markup-background-color:  #8B163FB4;
+--code-markup-border: 1px solid transparent;
+--message-box-transparency: #00000099;
+--embed-background-transparency: #00000099;
+--message-hover-buttons-background: #c8c8c81a;
+--context-menu-transparency: #0f0f0fcc;
+--add-discover-icon-transparency: #c8c8c826;
+--add-discover-icon-color: #8B163FB4;
+--channel-limit-count-color: #ffffffff;
+--channel-limit-background-left: #c8c8c81a;
+--channel-limit-background-right: #c8c8c833;
+--unread-message-divider-color: #f04747;
+--unread-message-divider-font-color: #ffffff;
+--new-messages-bar-background-color: #7289da;
+--new-messages-bar-font-color: #ffffff;
+--emoji-menu-sider-background-color: #8B163FFF;
+--emoji-menu-server-header-background-color: #8B163FFF;
+--emoji-menu-bottom-bar-background-color: #8B163FFF;
+--emoji-menu-search-bar-background-color: #8B163FFF;
+--message-search-menu-background-color: #8B163FB4;
+--message-search-container-background-color: #8B163FB4;
+--message-search-result-background-color: #8B163FB4;
+--message-search-result-background-color: #c8c8c80d;
+--text-voice-channel-color:  #FFFFFFFF;
+--text-voice-channel-hover-color:  #FFFFFFFF;
+--text-voice-channel-selected-text-color:  #FFFFFFFF;
+--unread-text-channel-color: #ffffff;
+--unread-text-channel-bubble-color: #ffffff;
+--folder-background-color: #8B163FFF;
+--expanded-folder-background-color: #8B163FB4;
+--reaction-emoji-background-color: #8B163FB4;
+--reaction-emoji-border-color: 1px solid #8B163FB4;
+--reaction-emoji-hover-background-color: ##8B163FB4;
+--reaction-emoji-hover-border-color: #8B163FB4;
+}
+
+.root-3JVdIJ {
+  background-color: rgba(151,11,59,0.7);
+}
+
+.scroller-3gAZLs list-6NCENv .thin-1ybCId .scrollerBase-289Jih {
+  background-color: #8B163FFF;
+}
+
+.header-8ilj5e{
+  background-color: #8B163FFF;
+}
+
+.contentWrapper-SvZHNd{
+  background-color: #8B163FFF;
+}
+
+.accountProfileCard-1XCH88 {
+  background-color: rgba(0,0,0, 0.3);
+}
+
+.emptyState-1C8By3 {
+  background-color: rgba(0,0,0, 0.3);
+}
+
+.contentRegionscroller-26nc1e {
+  background-color: rgba(0,0,0, 0.3);
+}
+
+.sidebarRegion-VFTUkN {
+  background-color: rgba(0,0,0, 0.3);
+}
